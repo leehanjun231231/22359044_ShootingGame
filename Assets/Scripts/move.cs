d7744e6e3934caf64a568f7090e9176b1d46dd9d
leehanjun231231 <leehanjun0825@kaywon.ac.kr>
2026-03-18 15:14:37 +0900

    Move + Instantiate

---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    public float spd = 5f;
+
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        //Vector3 direct = Vector3.right * h + Vector3.up * v;
+
+        //transform.Translate(direct * spd * Time.deltaTime);
+
+        Vector3 direct = new Vector3(h, v, 0);
+        transform.position = transform.position + direct * spd * Time.deltaTime;
+
+    }
+}
