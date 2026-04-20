07fcffc48a154ba8e14862fc5fd686daca849662
ajsdfsfafd <109218265+ajsdfsfafd@users.noreply.github.com>
2026-04-20 16:35:26 +0900

    중간 고사
    탄환 갯수를 10개로 변경 후 2초 뒤에 장전하도록 변경

--- Assets/Scripts/bulletFire.cs
+++ Assets/Scripts/bulletFire.cs
@@ -7,14 +7,43 @@ public class bulletFire : MonoBehaviour
     public GameObject bulletObject;
     public GameObject bulletFireObject;
 
+    public int maxBullet = 10;
+    private int currentBullet;
+    public float reloadTime = 2.0f;
+    private float reloadTimer = 0.0f;
+    private bool isReloading = false;
+
+    void Start()
+    {
+        currentBullet = maxBullet;
+    }
+
     private void Update()
     {
-        bool isFire = Input.GetButtonDown("Jump");
-        if (isFire)
+
+        if (isReloading)
+        {
+            reloadTimer += Time.deltaTime;
+            if (reloadTimer >= reloadTime)
+            {
+                currentBullet = maxBullet;
+                isReloading = false;
+                reloadTimer = 0.0f;
+            }
+        }
+
+        if (Input.GetButtonDown("Jump") && currentBullet > 0)
         {
             GameObject bullet = Instantiate(bulletObject);
             bullet.transform.position = bulletFireObject.transform.position;
+            currentBullet--;
         }
+
+        if (currentBullet <= 0)
+        {
+            isReloading = true;
+        }
+
     }
 
 
