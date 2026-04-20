e1c40b6badffd15eab1b663625090972419f3be4
ajsdfsfafd <109218265+ajsdfsfafd@users.noreply.github.com>
2026-04-20 14:58:36 +0900

    중간고사
    캐릭터가 아래로 너무 내려갈 경우 자동 사망

--- Assets/Scripts/CutLineWall.cs
+++ Assets/Scripts/CutLineWall.cs
@@ -8,6 +8,17 @@ public class CutLineWall : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Destroy(other.gameObject);
+
+        if (other.gameObject.tag == "Player")
+        {
+            GameObject gameManager = GameObject.Find("GameManager");
+            gameManager.GetComponent<GameOverManager>().ShowGameOver();
+
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+
+
     }
 
 }
