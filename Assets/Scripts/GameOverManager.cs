c44246355f5d4aca39e250010af7be9a2cbaceb3
ajsdfsfafd <109218265+ajsdfsfafd@users.noreply.github.com>
2026-04-20 15:53:49 +0900

    중간 고사
    게임 오버 시 효과음 추가
    게임 오버 시 배경음 소리 줄인다.

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -3,13 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class GameOverManager : MonoBehaviour
 {
-
+    public AudioSource bgmSource;
     public GameObject gameOverUI;
 
     public void ShowGameOver()
     {
         Time.timeScale = 0f;
         gameOverUI.SetActive(true);
+        bgmSource.volume = 0.1f;
+
     }
 
 
